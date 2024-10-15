40cad560918abce22d16c4043205334fca7b75c0
Beshoy <beshoy.ecladuos@onexpand.co.uk>
2024-10-15 22:58:19 +0300

    working on the paymob integration

--- modules/Checkout/Http/Controllers/PaymobController.php
+++ modules/Checkout/Http/Controllers/PaymobController.php
@@ -242,41 +242,45 @@ class PaymobController extends Controller
         
         $portal_id = $data['obj']['id'];
 
+        Order::where('id','1')->update([
+            'test_callback'=>$portal_id
+        ]);
 
-        if (/*$hased == $hmac && */ $data['obj']['success'] == "true") {
+
+        // if (/*$hased == $hmac && */ $data['obj']['success'] == "true") {
 
            
-            Order::where('payment_order_id',$portal_id)->update([
-                'status'=>'completed'
-            ]);
+        //     Order::where('payment_order_id',$portal_id)->update([
+        //         'status'=>'completed'
+        //     ]);
 
-            $order = Order::where('payment_order_id',$portal_id)->firstOrFail();
+        //     $order = Order::where('payment_order_id',$portal_id)->firstOrFail();
 
     
-            // $gateway = Gateway::get('paymob');
+        //     // $gateway = Gateway::get('paymob');
     
-            try {
-                // $response = $gateway->complete($order);
-                $trans = Transaction::create([
-                    'order_id' => $order['id'],
-                    'transaction_id' => $data['obj']['id'],
-                    'payment_method' => 'paymob',
-                    'created_at' => date('Y-m-d H:i:s'),
-                ]);
-                // event(new OrderPlaced($order));
-            } catch (Exception $e) {    
-                return response()->json([
-                    'message' => $e->getMessage(),
-                ], 403);
-            }
+        //     try {
+        //         // $response = $gateway->complete($order);
+        //         $trans = Transaction::create([
+        //             'order_id' => $order['id'],
+        //             'transaction_id' => $data['obj']['id'],
+        //             'payment_method' => 'paymob',
+        //             'created_at' => date('Y-m-d H:i:s'),
+        //         ]);
+        //         // event(new OrderPlaced($order));
+        //     } catch (Exception $e) {    
+        //         return response()->json([
+        //             'message' => $e->getMessage(),
+        //         ], 403);
+        //     }
     
     
-            if (!request()->ajax()) {
-                return redirect()->route('checkout.complete.show');
-            }
+        //     if (!request()->ajax()) {
+        //         return redirect()->route('checkout.complete.show');
+        //     }
 
 
-        }
+        // }
         
 
     }

cb09877deff498322935e9548cae692d122fe800
Beshoy <beshoy.ecladuos@onexpand.co.uk>
2024-10-23 00:11:40 +0300

    ignore updating images

--- node_modules/storefront-theme/Resources/lang/en/order_complete.php
+++ node_modules/storefront-theme/Resources/lang/en/order_complete.php
@@ -4,5 +4,5 @@ return [
     'order_placed' => 'Order Placed!',
     'your_order_has_been_placed' => 'Your order has been placed. Your order ID is: <b>:id</b>',
     'order_paid' => 'Order Paid!',
-    'your_order_has_been_paid' => 'Your order has been paid. Your order ID is: <b>:id</b>',
+    'your_order_has_been_paid' => 'Your order has been paid.</b>',
 ];
